51696ad29ff75e62870e330f72b893d4e858ad25
mail <mail@stefanwehr.de>
2011-10-08 14:58:53 +0200

    adjusted copyright date
    Ignore-this: c0cd4de630ec0fec67ebd8ebf88964e4
    
    darcs-hash:20111008125853-c96dc-575ae31c61ededbb1e141c3d5ae59b7804c45fea

--- Test/Framework/QuickCheckWrapper.hs
+++ Test/Framework/QuickCheckWrapper.hs
@@ -2,7 +2,7 @@
              DeriveDataTypeable,ScopedTypeVariables #-}
 
 --
--- Copyright (c) 2005,2009   Stefan Wehr - http://www.stefanwehr.de
+-- Copyright (c) 2005,2009-2011 Stefan Wehr - http://www.stefanwehr.de
 --
 -- This library is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU Lesser General Public
