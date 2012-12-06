3f70c6333c79ab8355e58c8e3286a9e7e7ce56dd
Stefan Wehr <mail@stefanwehr.de>
2012-12-07 00:20:38 +0100

    make tests running after extracting the .tar.gz file

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -8,11 +8,15 @@ cabal test
 cabal install
 
 cd tests
+mkdir -p dist/build/htfpp
+cp ../dist/build/htfpp/htfpp dist/build/htfpp
 cabal configure --enable-tests
 cabal build
 cabal test
 
 cd ../sample
+mkdir -p dist/build/htfpp
+cp ../dist/build/htfpp/htfpp dist/build/htfpp
 cabal configure --enable-tests
 cabal build
 cabal test
