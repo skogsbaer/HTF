84eb573fbeeee6ad5672f0f29c88a408a0c93afc
Stefan Wehr <mail@stefanwehr.de>
2015-11-12 09:14:50 +0100

    better diagnosis for test failing under travis

--- tests/compile-errors/run-tests.sh
+++ tests/compile-errors/run-tests.sh
@@ -6,12 +6,16 @@ lineno=7
 function check()
 {
     test="$1"
-    stack exec ghc -- -hide-package test-framework "$test" 2>&1 | grep "$test":$lineno
+    command -v grep >/dev/null 2>&1 || \
+        { echo >&2 "Test $0 requires ``grep'' but it's not installed.  Aborting."; exit 1; }
+    compile_cmd="stack exec ghc -- -hide-package test-framework $test"
+    $compile_cmd 2>&1 | grep "$test":$lineno
     grep_ecode=${PIPESTATUS[1]}
     if [ "$grep_ecode" != "0" ]
     then
         echo "Compile error for $test did not occur in line $lineno, exit code of grep: ${grep_ecode}"
-        stack exec ghc -- -hide-package test-framework "$test"
+        echo "Here is the output of the compiler:"
+        $compile_cmd
         exit 1
     fi
 }
