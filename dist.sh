29ec75fcd742d500781c8db7af89fdf43bc93b5f
Stefan Wehr <wehr@cp-med.com>
2017-08-06 16:59:26 +0200

    improve dist.sh script

--- dist.sh
+++ dist.sh
@@ -4,6 +4,9 @@ version=$(gawk -F: '/^Version:/ { print $2 }' HTF.cabal | sed 's/ //g')
 
 echo "Building version $version"
 
+# Somehow, --test-tarball always because of a strange permission error. That's why
+# we do the checks here by hand.
+
 stack sdist . --no-test-tarball || exit 1
 tarball=$(find $(pwd)/.stack-work/dist -name "*${version}.tar.gz" | xargs ls -t | head -1)
 
@@ -25,5 +28,10 @@ if ! git tag --points-at HEAD | grep -E "^$git_tag$" > /dev/null; then
     echo "Tagged HEAD with $git_tag"
 fi
 
-echo "All checks succeeded, now upload to archive using the following command:"
-echo "cabal upload ${tarball}"
+echo
+echo
+echo 'All checks succeeded, now upload to archive using the following command:'
+echo '$ stack upload . --no-test-tarball'
+echo
+echo 'And do not forget to push all tags:'
+echo '$ git push --tags'
