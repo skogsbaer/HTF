df81d5e9ed92e8cbd53c46c483460240bb3ee33d
Stefan Wehr <wehr@cp-med.com>
2019-11-25 12:22:42 +0100

    better logging for failing test

--- tests/compile-errors/run-tests.sh
+++ tests/compile-errors/run-tests.sh
@@ -9,11 +9,12 @@ function check()
     command -v grep >/dev/null 2>&1 || \
         { echo >&2 "Test $0 requires ``grep'' but it's not installed.  Aborting."; exit 1; }
     compile_cmd="stack $HTF_TRAVIS_STACK_ARGS ghc -- -XPackageImports $test"
-    $compile_cmd 2>&1 | grep "$test":$lineno
+    $compile_cmd 2>&1 | grep "$test":$lineno > /dev/null
     grep_ecode=${PIPESTATUS[1]}
     if [ "$grep_ecode" != "0" ]
     then
         echo "Compile error for $test did not occur in line $lineno, exit code of grep: ${grep_ecode}"
+        echo "Compile command: $compile_cmd"
         echo "Here is the output of the compiler:"
         $compile_cmd
         exit 1
