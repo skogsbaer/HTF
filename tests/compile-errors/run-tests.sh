c77a21927fa83f9dd6824c91c1e5e846c9d3f55c
Stefan Wehr <wehr@cp-med.com>
2019-11-25 13:39:00 +0100

    make test work for stack >= 2

--- tests/compile-errors/run-tests.sh
+++ tests/compile-errors/run-tests.sh
@@ -8,13 +8,15 @@ function check()
     test="$1"
     command -v grep >/dev/null 2>&1 || \
         { echo >&2 "Test $0 requires ``grep'' but it's not installed.  Aborting."; exit 1; }
-    compile_cmd="stack $HTF_TRAVIS_STACK_ARGS ghc -- -XPackageImports $test"
+    compile_cmd="stack $HTF_TRAVIS_STACK_ARGS ghc --package HTF -- -XPackageImports $test"
     $compile_cmd 2>&1 | grep "$test":$lineno > /dev/null
     grep_ecode=${PIPESTATUS[1]}
     if [ "$grep_ecode" != "0" ]
     then
         echo "Compile error for $test did not occur in line $lineno, exit code of grep: ${grep_ecode}"
         echo "Compile command: $compile_cmd"
+        echo "Stack version: $(stack --version)"
+        echo "GHC version: $(stack $HTF_TRAVIS_STACK_ARGS ghc -- --version)"
         echo "Here is the output of the compiler:"
         $compile_cmd
         exit 1
