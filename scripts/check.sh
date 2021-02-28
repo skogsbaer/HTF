11d4368f3079f4d21dee64f83b2dc840d19fdfd4
Stefan Wehr <stefan.wehr@hs-offenburg.de>
2021-02-28 17:32:34 +0100

    rename HTF_TRAVIS_STACK_ARGS into HTF_TEST_STACK_ARGS and improve debugging support

--- scripts/check.sh
+++ scripts/check.sh
@@ -14,7 +14,7 @@ if [ ! -e "$TRAVIS" ]; then
     exit 1
 fi
 
-YAMLS=$(grep '^- HTF_TRAVIS_STACK_ARGS' "$TRAVIS" | sed 's/^.*="--stack-yaml //g; s/"$//g')
+YAMLS=$(grep '^- HTF_TEST_STACK_ARGS' "$TRAVIS" | sed 's/^.*="--stack-yaml //g; s/"$//g')
 
 if [ -z "$TRAVIS_BUILD_DIR" ]; then
     export TRAVIS_BUILD_DIR="$TOP"
@@ -24,14 +24,14 @@ for yaml in $YAMLS; do
     echo >&2
     eval yaml="$yaml" # evaluate variables contained in $yaml
     echo >&2 "Checking with $yaml ..."
-    export HTF_TRAVIS_STACK_ARGS="--stack-yaml $yaml"
-    stack $HTF_TRAVIS_STACK_ARGS build
+    export HTF_TEST_STACK_ARGS="--stack-yaml $yaml"
+    stack $HTF_TEST_STACK_ARGS build
     ecode=$?
     if [ $ecode -ne 0 ]; then
         echo >&2 "Build for $yaml failed!"
         exit 1
     fi
-    stack $HTF_TRAVIS_STACK_ARGS test
+    stack $HTF_TEST_STACK_ARGS test
     ecode=$?
     if [ $ecode -ne 0 ]; then
         echo >&2 "Test for $yaml failed!"
--- tests/compile-errors/run-tests.sh
+++ tests/compile-errors/run-tests.sh
@@ -9,7 +9,7 @@ function check()
     echo "Running test $test ..."
     command -v grep >/dev/null 2>&1 || \
         { echo >&2 "Test $0 requires ``grep'' but it's not installed.  Aborting."; exit 1; }
-    compile_cmd="stack $HTF_TRAVIS_STACK_ARGS ghc --package HTF -- -XPackageImports $test"
+    compile_cmd="stack $HTF_TEST_STACK_ARGS ghc --package HTF -- -XPackageImports $test"
     $compile_cmd 2>&1 | grep "$test":$lineno > /dev/null
     grep_ecode=${PIPESTATUS[1]}
     if [ "$grep_ecode" != "0" ]
@@ -17,7 +17,7 @@ function check()
         echo "Compile error for $test did not occur in line $lineno, exit code of grep: ${grep_ecode}"
         echo "Compile command: $compile_cmd"
         echo "Stack version: $(stack --version)"
-        echo "GHC version: $(stack $HTF_TRAVIS_STACK_ARGS ghc -- --version)"
+        echo "GHC version: $(stack $HTF_TEST_STACK_ARGS ghc -- --version)"
         echo "Here is the output of the compiler:"
         $compile_cmd
         exit 1
